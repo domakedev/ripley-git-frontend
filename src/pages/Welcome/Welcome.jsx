6805fab7d8122a8383bf114f6df47604b9edf317
Cesar Guevara Cabrera <sarcansino@gmail.com>
2022-03-06 21:46:16 -0500

    Update Welcome.jsx

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -2,21 +2,17 @@ import React from 'react'
 import MainLayout from '../../layouts/MainLayout/MainLayout'
 import './Welcome.css'
 
-const Welcome = () => {
-    const asd = 1
-    console.log('🚀 ~ file: Welcome.jsx ~ line 6 ~ asd', asd)
-    return (
-        <MainLayout>
-            <div className="welcome-main">
-                <h1 className="welcome-gretting">
-                    ¡Hola <strong> Admin!</strong>
-                </h1>
-                <p className="welcome-description">
-                    Añade y ve a tus cliente, todo en un mismo lugar.
-                </p>
-            </div>
-        </MainLayout>
-    )
-}
+const Welcome = () => (
+    <MainLayout>
+        <div className="welcome-main">
+            <h1 className="welcome-gretting">
+                ¡Hola <strong> Admin!</strong>
+            </h1>
+            <p className="welcome-description">
+                Añade y ve a tus cliente, todo en un mismo lugar.
+            </p>
+        </div>
+    </MainLayout>
+)
 
 export default Welcome
